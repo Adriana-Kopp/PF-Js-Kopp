61146173d6f021e01619866a9f50094d8fc24c27
Adriana Kopp <acm.kopp@gmail.com>
2023-01-26 16:28:34 +0100

    Extras

--- data/stock.js
+++ data/stock.js
@@ -1,68 +1,67 @@
-const productos =
-[
-    {
-        id: 1,
-        nombre: "Buzo 1",
-        tipo: "buzo",
-        desc: "Un buzo que re va con vos",
-        precio: 1200,
-        talle: "L",
-        imagen: '../public/images/buzo.jpg',
-        cantidad:1
-    },
+const productos = [
+  {
+    id: 1,
+    nombre: "Lavanda",
+    tipo: "lavanda",
+    desc: "Es cicatrizante y antiséptico. Tiene propiedades relajantes, ansiolíticos y sedantes. Analgésico natural.",
+    precio: 1200,
+    gramos: "150",
+    imagen: "../public/images/lavanda.jpg",
+    cantidad: 1,
+  },
 
-    {
-        id: 2,
-        nombre: "Remera 1",
-        tipo: "remera",
-        desc: "Una remera que re va con vos",
-        precio: 500,
-        talle: "L",
-        imagen: '../public/images/remera.jpg',
-        cantidad:1
-    },
+  {
+    id: 2,
+    nombre: "Manzanilla",
+    tipo: "manzanilla",
+    desc: "Sirve para trastornos digestivos, afecciones renales y de la vejiga, dolores menstruales.",
+    precio: 600,
+    gramos: "200",
+    imagen: "../public/images/manzanilla.jpg",
+    cantidad: 1,
+  },
 
-    {
-        id: 3,
-        nombre: "Camisa 1",
-        tipo: "camisa",
-        desc: "Una camisa que re va con vos",
-        precio: 900,
-        talle: "S",
-        imagen: '../public/images/camisa.jpg',
-        cantidad:1
-    },
+  {
+    id: 3,
+    nombre: "Canela",
+    tipo: "canela",
+    desc: "´Por su poder antioxidante se ha utilizado durante muchísimo tiempo como conservante natural. Asimismo, es expectorante y posee propiedades analgésicas.",
+    precio: 400,
+    gramos: "250",
+    imagen: "../public/images/canela.jpg",
+    cantidad: 1,
+  },
 
-    {
-        id: 4,
-        nombre: "Pantalon 1",
-        tipo: "pantalon",
-        desc: "Una pantalon que re va con vos",
-        precio: 1600,
-        talle: "L",
-        imagen: '../public/images/pantalon.jpg',
-        cantidad:1
-    },
+  {
+    id: 4,
+    nombre: "Aciano Azul",
+    tipo: "aciano azul",
+    desc: "Posee aplicaciones como aperitiva, diurética, antibiótica, antipirética y antiinflamatoria.",
+    precio: 1200,
+    gramos: "200",
+    imagen: "../public/images/aciano-azul.jpg",
+    cantidad: 1,
+  },
 
-    {
-        id: 5,
-        nombre: "Buzo 2",
-        tipo: "buzo",
-        desc: "Un buzo que re va con vos",
-        precio: 1200,
-        talle: "L",
-        imagen: '../public/images/buzo.jpg',
-        cantidad:1
-    },
+  {
+    id: 5,
+    nombre: "Abedul",
+    tipo: "abedul hojas naturales",
+    desc: "Utilizado en fitoterapia por sus propiedades medicinales para la elaboración de productos ecológicos de cosmética y alimentación.",
+    precio: 700,
+    gramos: "150",
+    imagen: "../public/images/abedul.jpg",
+    cantidad: 1,
+  },
 
-    {
-        id: 6,
-        nombre: "Remera 2",
-        tipo: "remera",
-        desc: "Una remera que re va con vos",
-        precio: 500,
-        talle: "L",
-        imagen: '../public/images/remera.jpg',
-        cantidad:1
-    }
+  {
+    id: 6,
+    nombre: "Menta",
+    tipo: "menta",
+    desc: "Reduce la congestión nasal, de la garganta y los pulmones. También es eficaz en el tratamiento de la tos, la bronquitis y el asma, gracias a su efecto expectorante.",
+    precio: 500,
+    gramos: "200",
+    imagen: "../public/images/menta.jpg",
+    cantidad: 1,
+  },
 ];
