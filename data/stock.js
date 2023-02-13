c7a9ce7ae0e0e5813cf11323b4d1fce17d169cc1
Adriana Kopp <acm.kopp@gmail.com>
2023-02-13 14:37:59 +0100

    Extras

--- data/stock.js
+++ data/stock.js
@@ -6,7 +6,7 @@ const productos = [
     desc: "Es cicatrizante y antiséptico. Tiene propiedades relajantes, ansiolíticos y sedantes. Analgésico natural.",
     precio: 1200,
     gramos: "150",
-    imagen: "./public/images/lavanda.jpg",
+    imagen: "../public/images/lavanda.jpg",
     cantidad: 1,
   },
 
@@ -17,7 +17,7 @@ const productos = [
     desc: "Sirve para trastornos digestivos, afecciones renales y de la vejiga, dolores menstruales.",
     precio: 600,
     gramos: "200",
-    imagen: "./public/images/manzanilla.jpg",
+    imagen: "../public/images/manzanilla.jpg",
     cantidad: 1,
   },
 
@@ -28,7 +28,7 @@ const productos = [
     desc: "Por su poder antioxidante se ha utilizado durante muchísimo tiempo como conservante natural. Asimismo, es expectorante y posee propiedades analgésicas.",
     precio: 400,
     gramos: "250",
-    imagen: "./public/images/canela.jpg",
+    imagen: "../public/images/canela.jpg",
     cantidad: 1,
   },
 
@@ -39,7 +39,7 @@ const productos = [
     desc: "Posee aplicaciones como aperitiva, diurética, antibiótica, antipirética y antiinflamatoria.",
     precio: 1200,
     gramos: "200",
-    imagen: "./public/images/aciano-azul.jpg",
+    imagen: "../public/images/aciano-azul.jpg",
     cantidad: 1,
   },
 
@@ -50,7 +50,7 @@ const productos = [
     desc: "Utilizado en fitoterapia por sus propiedades medicinales para la elaboración de productos ecológicos de cosmética y alimentación.",
     precio: 700,
     gramos: "150",
-    imagen: "./public/images/abedul.jpg",
+    imagen: "../public/images/abedul.jpg",
     cantidad: 1,
   },
 
@@ -61,7 +61,7 @@ const productos = [
     desc: "Reduce la congestión nasal, de la garganta y los pulmones. También es eficaz en el tratamiento de la tos, la bronquitis y el asma, gracias a su efecto expectorante.",
     precio: 500,
     gramos: "200",
-    imagen: "./public/images/menta.jpg",
+    imagen: "../public/images/menta.jpg",
     cantidad: 1,
   },
 ];
